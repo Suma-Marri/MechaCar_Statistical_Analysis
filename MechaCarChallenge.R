6e32d40961b0af471e869da62f1f7b7d6cf5cf17
Suma Marri <smarri0612@gmail.com>
2022-04-01 19:08:41 -0400

    Update MechaCarChallenge.R

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -24,3 +24,16 @@ total_summary <- mecha_coil %>% summarize(Mean_PSI=mean(PSI), Median_PSI=median(
 # Create a lot_summary dataframe using the group_by() and the summarize() functions to group each manufacturing lot.                                                                
 lot_summary <- mecha_coil  %>% group_by(Manufacturing_Lot) %>% summarize(Mean_PSI=mean(PSI), Median_PSI=median(PSI), Var_PSI=var(PSI),Std_Dev_PSI=sd(PSI),Num_Coil=n(), .groups = 'keep') 
 
+### DELIVERABLE 3
+
+# Using the t.test function to determine if the PSI across ALL lots is statistically different from the pop. mean of 1,500 PSI.
+t.test(mecha_coil$PSI,mu=1500)
+
+# Using t.test function and subset function to determine if PSI for each manufacturing lot is statistically different from the pop. mean of 1,500 PSI
+lot1 <- subset(mecha_coil, Manufacturing_Lot=="Lot1")
+lot2 <- subset(mecha_coil, Manufacturing_Lot=="Lot2")
+lot3 <- subset(mecha_coil, Manufacturing_Lot=="Lot3")
+
+t.test(lot1$PSI,mu=1500)
+t.test(lot2$PSI,mu=1500)
+t.test(lot3$PSI,mu=1500)
